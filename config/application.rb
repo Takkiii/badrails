a65ae4e1cba8f234dc8d0d90ca677efb309ce3e4
瀧ヶ崎翔 <Takkiii@Takkiii.local>
2017-01-18 17:25:03 +0900

    add devise_gem

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module Badrails
   class Application < Rails::Application
     config.assets.paths << config.root.join("node_modules")
+    config.i18n.default_locale = :ja
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
