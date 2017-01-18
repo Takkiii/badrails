ea7c671dab3e58d7ab9fc924f20563145d001d31
瀧ヶ崎翔 <Takkiii@Takkiii.local>
2017-01-18 18:10:10 +0900

    add jquery-ujs_to_package.json

--- app/client/javascripts/application.js
+++ app/client/javascripts/application.js
@@ -1,11 +1,13 @@
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
 /**
  * Import Twitter bootstrap
  */
 require('imports?jQuery=jquery!bootstrap-sass/assets/javascripts/bootstrap');
 require('font-awesome-sass-loader');
+
+/**
+ * Import Jquery
+ */
+require('imports?jQuery=jquery!jquery-ujs');
 
 /**
  * Import stylesheet
